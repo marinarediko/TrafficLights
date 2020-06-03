2af853a174a38cf7ef23564358cb721c584cd13b
marinarediko <ma.rediko@gmail.com>
2020-06-03 19:09:20 +0300

    Version_RunningLights_01

--- Form1.cs
+++ Form1.cs
@@ -13,17 +13,70 @@ namespace TrafficLights
     public partial class TrafficLights : Form
     {
         private Timer timerSwitch;
-        
-        
-        
+        private bool R_to_G = true;
+        private int tickCount = 0;
+        private bool greenOff = true;
+               
         public TrafficLights()
         {
             InitializeComponent();
+            InitializeTrafficLight();
+            InitializeTimerSwitch();
+        }
+
+        private void InitializeTimerSwitch()
+        {
+            timerSwitch = new Timer();
+            timerSwitch.Interval = 250;
+            timerSwitch.Tick += new EventHandler(TimerSwitchTick);
+            timerSwitch.Start();
+        }
+
+        private void TimerSwitchTick(object sender, EventArgs e)
+        {
+            if (tickCount >= 14 && tickCount < 20 && !greenOff)
+            {
+                if (GreenLight.BackColor == Color.Gray)
+                    GreenLight.BackColor = Color.Green;
+                else
+                    GreenLight.BackColor = Color.Gray;
+                tickCount++;
+            }
+            else if (tickCount == 20)
+            {
+                if (RedLight.BackColor == Color.Red)
+                {
+                    RedLight.BackColor = Color.Gray;
+                    YellowLight.BackColor = Color.Yellow;
+                }
+                else if (YellowLight.BackColor == Color.Yellow && R_to_G)
+                {
+                    YellowLight.BackColor = Color.Gray;
+                    GreenLight.BackColor = Color.Green;
+                    R_to_G = false;
+                    greenOff = false;
+                }
+                else if (YellowLight.BackColor == Color.Yellow && !R_to_G)
+                {
+                    YellowLight.BackColor = Color.Gray;
+                    RedLight.BackColor = Color.Red;
+                    R_to_G = true;
+                }
+                else if (GreenLight.BackColor == Color.Green)
+                {
+                    YellowLight.BackColor = Color.Yellow;
+                    GreenLight.BackColor = Color.Gray;
+                    greenOff = true;
+                }
+                tickCount = 0;
+            }
+            else
+                tickCount++;
         }
 
         private void InitializeTrafficLight()
         {
-            RedLight.BackColor = Color.Gray;
+            RedLight.BackColor = Color.Red;
             YellowLight.BackColor = Color.Gray;
             GreenLight.BackColor = Color.Gray;
         }
